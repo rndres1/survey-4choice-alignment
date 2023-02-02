c8f953034345304072a5132f4fff7ffff4dffb6a
Sohhom Bandyopadhyay <sohhom.bandyopadhyay@playpowerlabs.com>
2023-02-02 19:08:18 +0530

    working demo

--- experiment.js
+++ experiment.js
@@ -1,24 +1,42 @@
 
+const n_trials = 5;
+
+// ======== END OF PARAMS =======
+
 const jsPsych = initJsPsych({
     on_finish: function() {
     jsPsych.data.displayData();
 }
 });
 
+function random_sample_without_replacement(sample_size, population_size) {
+    const arr = [];
+    while(arr.length < sample_size){
+        var r = Math.floor(Math.random() * population_size) + 1;
+        if(arr.indexOf(r) === -1) {
+            arr.push(r);
+        }
+    }
+    return(arr)
+}
+
+image_names = filenames.map(i => './media/' + i);
 var timeline = [];
 
-var trial1 = {
-    type: myplugin2, 
-    image_fnames: [image_names[0], image_names[0], image_names[0], image_names[0]],
-    instruction: "Which image is the best?"
+for (i=0;i<n_trials;i++)
+{
+    rindx = random_sample_without_replacement(4,filenames.length);
+    image_set = [image_names[rindx[0]], image_names[rindx[1]], image_names[rindx[2]], image_names[rindx[3]]]
+    var trial1 = {
+        type: myplugin2, 
+        image_fnames: image_set,
+        instruction: "Which image is the best?"
+    }
+    timeline.push(trial1);
 }
-timeline.push(trial1);
 
-var trial2 = {
-    type: myplugin2, 
-    image_fnames: [image_names[0], image_names[0], image_names[0], image_names[0]],
-    instruction: "Which image is the best?"
-}
-timeline.push(trial2);
 
-jsPsych.run(timeline)
+
+
+
+jsPsych.run(timeline);
